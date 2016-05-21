fd664152ce3913a5ad7f47d78fd1cb3491db95fc
Hlch1 <Hlch19921228@gmail.com>
2016-05-21 11:32:26 -0400

    removed args from main loaded int array

---
+++ Chapter7/src/chapter7/ChapterSeven.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter7;
+
+/**
+ *
+ * @author Hlch1
+ */
+public class ChapterSeven
+{
+
+    public static void main(String[] args)
+    {
+        InitArray ina = new InitArray(new int[] {5,0,4});
+                ina.makeArray();
+    }
+}
