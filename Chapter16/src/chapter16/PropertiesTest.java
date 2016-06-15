72b312ed51f6c622671afc94106eb9ef03603c24
Hlch1 <Hlch19921228@gmail.com>
2016-06-14 21:39:52 -0400

    Chapter16

--- Chapter16/src/chapter16/PropertiesTest.java
+++ Chapter16/src/chapter16/PropertiesTest.java
@@ -10,83 +10,95 @@ package chapter16;
  * @author Hlch1
  */
 import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.Set;
-
-public class PropertiesTest
-{
-    public PropertiesTest()
-    {
-        Properties table = new Properties();
-        
-        table.setProperty("color", "blue");
-        table.setProperty("width", "200");
-        
-        System.out.println("After setting properties");
-        listProperties(table);
-        
-        table.setProperty("color", "red");
-        
-        System.out.println("After replacing properties");
-        listProperties(table);
-        
-        saveProperties(table);
-        
-        table.clear();
-        
-        System.out.println("After clearing properties");
-        listProperties(table);
-        
-        loadProperties(table);
-        
-        Object value = table.getProperty("color");
-        
-        if (value != null)
-            System.out.printf("Property color value is %s%n", value);
-        else
+    import java.io.FileInputStream;
+    import java.io.IOException;
+    import java.util.Properties;
+    import java.util.Set;
+  
+    public class PropertiesTest
+   {
+      public PropertiesTest()
+      {
+         Properties table = new Properties();
+ 
+         // set properties                  
+         table.setProperty("color", "blue");
+         table.setProperty("width", "200"); 
+ 
+         System.out.println("After setting properties");
+         listProperties(table);
+ 
+         // replace property value         
+         table.setProperty("color", "red");
+ 
+         System.out.println("After replacing properties");
+         listProperties(table);
+ 
+         saveProperties(table);
+ 
+         table.clear(); // empty table 
+ 
+         System.out.println("After clearing properties");
+         listProperties(table);
+ 
+         loadProperties(table);
+ 
+         // get value of property color            
+         Object value = table.getProperty("color");
+ 
+         // check if value is in table
+         if (value != null)
+            System.out.printf("Property color's value is %s%n", value);
+         else
             System.out.println("Property color is not in table");
-    }
-    
-    private static void saveProperties(Properties props)
-    {
-        try
-        {
+      }
+ 
+      // save properties to a file
+      private static void saveProperties(Properties props)
+      {
+         // save contents of table
+         try
+         {
             FileOutputStream output = new FileOutputStream("props.dat");
+            props.store(output, "Sample Properties"); // save properties 
             output.close();
             System.out.println("After saving properties");
             listProperties(props);
-        }
-        catch (IOException ioException)
-        {
+         }
+         catch (IOException ioException)
+         {
             ioException.printStackTrace();
-        }
-    }
-    
-    private static void loadProperties(Properties props)
-    {
-        try
-        {
+         }
+      }
+ 
+      // load properties from a file
+      private static void loadProperties(Properties props)
+      {
+         // load contents of table
+         try
+         {
             FileInputStream input = new FileInputStream("props.dat");
-            props.load(input);
+            props.load(input); // load properties 
             input.close();
             System.out.println("After loading properties");
             listProperties(props);
-        }
-        catch (IOException ioException)
-        {
+         }
+         catch (IOException ioException)
+         {
             ioException.printStackTrace();
-        }
-    }
-    
-    private static void listProperties(Properties props)
-    {
-        Set<Object> keys = props.keySet();
-        
-        for (Object key : keys)
-            System.out.printf("%s\t%s%n", key, props.getProperty((String) key));
-        
-        System.out.println();
-    }
-}
+         }
+      }
+ 
+      // output property values
+      private static void listProperties(Properties props)
+      {
+         Set<Object> keys = props.keySet(); // get property names 
+ 
+         // output name/value pairs
+         for (Object key : keys)
+            System.out.printf(
+               "%s\t%s%n", key, props.getProperty((String) key));
+ 
+         System.out.println();
+      }
+  }
